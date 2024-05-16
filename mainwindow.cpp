13787f89ef13cf1eed830ba3b4b69f7496da5c8f
filnow <filnow3@st.amu.edu.pl>
2024-05-16 19:24:14 +0200

    feat: load image, display image, display window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDir>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +16,20 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Open a file", QDir::homePath(),
+                            tr("Image Files (*.png *.jpg *.bmp *.tif);;"));
+
+    // Check if file name empty
+    if (file_name != "") {
+        // Display image on label
+        QPixmap img(file_name);
+        ui->label_pic->setPixmap(img);
+
+        // MessageBox with path to file
+        QMessageBox::information(this, "Displayed image from path:", file_name);
+    }
+}
+
