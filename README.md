408be2a99424e5c0c5fba4ec0e2191045486bfba
filnow <filnow3@st.amu.edu.pl>
2024-05-17 21:48:09 +0200

    add sam model, opencv works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dex-vision
+
+
+TODO:
+  - add ONNX SAM [model](https://github.com/facebookresearch/segment-anything/blob/main/scripts/export_onnx_model.py)
+ 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,11 +5,22 @@
 #include <QDir>
 #include <QPixmap>
 
+#include <atomic>
+#include <opencv2/opencv.hpp>
+#include <thread>
+
+#define STRIP_FLAG_HELP 1
+#include <gflags/gflags.h>
+
+#include "sam.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // Set label background color to grey
+    ui->label_pic->setStyleSheet("QLabel { background-color : grey;}");
 }
 
 MainWindow::~MainWindow()
@@ -31,5 +42,6 @@ void MainWindow::on_pushButton_clicked()
         // MessageBox with path to file
         QMessageBox::information(this, "Displayed image from path:", file_name);
     }
+
 }
 
